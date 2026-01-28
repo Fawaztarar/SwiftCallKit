72a979c59d734f1257a267abdb957d1b257b2f96
Fawaz Tarar <fawaztarar@gmail.com>
2026-01-28 00:33:48 +0000

    Initial SwiftCallkit project

---
+++ SwiftCallKit/SwiftCallKit/SwiftCallKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCallKitApp.swift
+//  SwiftCallKit
+//
+//  Created by Fawaz Tarar on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCallKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
